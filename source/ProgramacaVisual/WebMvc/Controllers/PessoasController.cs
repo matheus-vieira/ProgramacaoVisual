55473bc122a7b64bd9497f254866d0a29ab98de3
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2018-03-13 12:54:56 +0000

    Initial commit

--- source/ProgramacaVisual/WebMvc/Controllers/PessoasController.cs
+++ source/ProgramacaVisual/WebMvc/Controllers/PessoasController.cs
@@ -32,5 +32,25 @@ namespace WebMvc.Controllers
             // visualização dos dados
             return View(viewModel);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(
+            [Bind("Nome")]Pessoa newPessoa
+        )
+        {
+            if (!ModelState.IsValid)
+                return View(newPessoa);
+            
+            newPessoa.Id = pessoaList.Max(p => p.Id) + 1;
+            pessoaList.Add(newPessoa);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
