4d4d2b29cf5415b4745d81006c29f40cce3f8b33
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2018-03-20 11:15:55 -0300

    Adicionado ações para editar uma pessoa

--- source/ProgramacaVisual/WebMvc/Controllers/PessoasController.cs
+++ source/ProgramacaVisual/WebMvc/Controllers/PessoasController.cs
@@ -52,5 +52,77 @@ namespace WebMvc.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+                return NotFound();
+
+            var pessoa = pessoaList
+                .FirstOrDefault(p => p.Id == id);
+
+            if (pessoa == null)
+                return NotFound();
+
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(
+            [Bind("Id, Nome")]Pessoa editPessoa
+        )
+        {
+            if (!ModelState.IsValid)
+                return View(editPessoa);
+            
+            var pessoaToEdit = pessoaList
+                .FirstOrDefault(p => p.Id == editPessoa.Id);
+
+            if (pessoaToEdit == null)
+                return NotFound();
+
+            pessoaToEdit.Nome = editPessoa.Nome;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
+
+
+
+        private Pessoa First(int id) {
+            for (int i = 0; i < pessoaList.Count; i++)
+                if (pessoaList[i].Id == id)
+                    return pessoaList[i];
+            throw new InvalidOperationException("Sequence contain....");
+        }
+
+        private Pessoa FirstOrDefault(int id) {
+            for (int i = 0; i < pessoaList.Count; i++)
+                if (pessoaList[i].Id == id)
+                    return pessoaList[i];
+            return null;
+        }
+
+        private Pessoa FirstWithAction(Func<Pessoa, bool> action) {
+            for (int i = 0; i < pessoaList.Count; i++)
+                if (action(pessoaList[i]))
+                    return pessoaList[i];
+            return null;
+        }
+
+        private Pessoa FirstCallAction(int id) {
+            //return FirstWithAction(p => CheckPessoa(p, id));
+            return FirstWithAction(p => p.Id == id);
+        }
+
+        private bool CheckPessoa(Pessoa pessoa, int id) {
+            return pessoa.Id == id;
+        }
     }
 }
